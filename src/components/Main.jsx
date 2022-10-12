feebcdacc2ce20f29d18970852f3c61349ff79b0
ed <edwardhaddican@gmail.com>
2022-10-12 09:41:12 -0400

    starter code

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div id="main">
+    <div id="navbar">
+      <div>Contact List</div>
+    </div>
+    <div id="container">
+      {/* your components here */}
+    </div>
+  </div>
+  );
+};
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from "react-dom/client"
+import {Main} from './components'
+
+
+const container = document.getElementById('app')
+const root = ReactDOM.createRoot(container)
+
+root.render(<Main />)
