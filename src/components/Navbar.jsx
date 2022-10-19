9970e207ebdeeaa7a29a30e78be7d65e7bece31f
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-10-19 19:38:02 -0400

    added pseudo code comments to provide web design goals for navigation links to be hidden until selected

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,12 @@ const Navbar = () => {
     <div id="navbar">
       <h1>insert logo or img </h1>
       <NavLink to="login">Login</NavLink>
+      {/* login to be up upon page load */}
       <NavLink to="register">Register</NavLink>
+      {/* set to only show after selected to "Create account" with a link */}
       <NavLink to="posts">Posts</NavLink>
       <NavLink to="posts">Profile</NavLink>
+      {/* set to show only after successfully logged in */}
 
       {/* <Login />
       <Register /> */}
