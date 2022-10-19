cf8b94bd67bdf1b6303af2263af223a7283979d1
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-10-19 15:09:03 -0400

    added useState for login state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Navbar } from "./";
 import Posts from "./Posts";
 import Register from "./Register";
@@ -6,6 +6,15 @@ import Login from "./Login";
 import { Routes, Route } from "react-router-dom";
 
 const Main = () => {
+  const [islogin, SetLogin] = useState(false)
+  // useEffect(() => {
+  //   async function fetchPosts() {
+  //     const allPosts = await getPosts();
+  //     setAllPosts(allPosts);
+  //   }
+  //   fetchPosts();
+  // }, []);
+
   return (
     <div id="main">
       <Navbar />
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Posts } from "./Posts";
 export { default as Register } from "./Register";
 export {default as SinglePost} from "./SinglePost";
+export {default as Profile} from "./Profile";
