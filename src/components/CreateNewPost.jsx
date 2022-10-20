04809ec01d694c31340ac5a1b7170776651c9feb
Dillan May <dillanmay_6@yahoo.com>
2022-10-20 13:59:49 -0500

    created all the useState functions for the create a new post

--- src/components/CreateNewPost.jsx
+++ src/components/CreateNewPost.jsx
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 
 const CreateNewPost = (props) => {
   const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [location, setLocation] = useState("");
+  const [willDeliver, setWillDeliver] = useState("");
+
+
+
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -11,6 +18,23 @@ const CreateNewPost = (props) => {
     console.log(e.target.value);
     setTitle(e.target.value);
   }
+  function handleDescriptionChange(e) {
+    console.log(e.target.value);
+    setDescription(e.target.value);
+  }
+  function handlePriceChange(e) {
+    console.log(e.target.value);
+    setPrice(e.target.value);
+  }
+  function handleLocationChange(e) {
+    console.log(e.target.value);
+    setLocation(e.target.value);
+  }
+  function handleWillDeliverChange(e) {
+    console.log(e.target.value);
+    setWillDeliver(e.target.value);
+  }
+
   console.log(title);
   return (
     <form onSubmit={handleSubmit}>
@@ -20,11 +44,19 @@ const CreateNewPost = (props) => {
       </label>
       <label>
         Description:
-        <input id="description" onChange={handleTitleChange} defaultValue={title} />
+        <input id="description" onChange={handleDescriptionChange} defaultValue={description} />
       </label>
       <label>
-        title:
-        <input id="title" onChange={handleTitleChange} defaultValue={title} />
+        Price:
+        <input id="price" onChange={handlePriceChange} defaultValue={price} />
+      </label>
+      <label>
+        Location:
+        <input id="location" onChange={handleLocationChange} defaultValue={location} />
+      </label>
+      <label>
+        Will Deliver:
+        <input id="willDeliver" onChange={handleWillDeliverChange} defaultValue={willDeliver} />
       </label>
       {/* <input id="description" defaultValue={formDetails.description} />
       <input id="price" defaultValue={formDetails.price} />
